4abfd347f7bc9aa6cfdfee0dc15e943fa8703163
centurionbattles <arunsebastian.stat@gmail.com>
2025-08-06 20:00:30 +0100

    Update game-loop.js

--- game-loop.js
+++ game-loop.js
@@ -1,4 +1,4 @@
 import { createEmptyBoard, setupPieces, board } from './board-setup.js';
 import { renderBoard } from './render.js';
 import { onSquareClick } from './interaction.js';
-
+initGame();
